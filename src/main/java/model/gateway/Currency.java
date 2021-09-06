780369bbfce3209e3739b8c4db52e5d9b0dff7c0
Musobek253 <musobek253@gmail.com>
2021-09-06 22:04:53 +0300

    P2p

--- src/main/java/model/gateway/Currency.java
+++ src/main/java/model/gateway/Currency.java
@@ -1,4 +1,18 @@
 package model.gateway;
 
 public enum Currency {
+
+    RUB("RUBL", 643),
+    EURO("EUR",978),
+    SUM("SUM",860),
+    USD("USD",840);
+
+
+    Currency(String code, Integer number) {
+        this.code = code;
+        this.number = number;
+    }
+
+    private String code;
+    private Integer number;
 }
--- src/main/java/model/gateway/Gateway.java
+++ src/main/java/model/gateway/Gateway.java
@@ -1,4 +1,118 @@
 package model.gateway;
 
+import java.math.BigDecimal;
+
 public class Gateway {
+    private int id;
+    private String name;
+    private Currency currency;
+    private BigDecimal minAmount;
+    private BigDecimal maxAmount;
+    private BigDecimal commissionUp;
+    private BigDecimal commissionDown;
+    private int merchantId;
+
+    private Boolean isUcell;
+    private Boolean isBeeline;
+    private Boolean isWebmoney;
+    private Boolean isQiwi;
+
+    public Gateway(int id, String name, Currency currency, BigDecimal minAmount, BigDecimal maxAmount, BigDecimal commissionUp, BigDecimal commissionDown, int merchantId) {
+        this.id = id;
+        this.name = name;
+        this.currency = currency;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
+        this.commissionUp = commissionUp;
+        this.commissionDown = commissionDown;
+        this.merchantId = merchantId;
+    }
+
+    public Gateway(int id, String name, Currency currency, BigDecimal minAmount, BigDecimal maxAmount) {
+        this.id = id;
+        this.name = name;
+        this.currency = currency;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getMinAmount() {
+        return minAmount;
+    }
+
+    public void setMinAmount(BigDecimal minAmount) {
+        this.minAmount = minAmount;
+    }
+
+    public BigDecimal getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(BigDecimal maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public BigDecimal getCommissionUp() {
+        return commissionUp;
+    }
+
+    public void setCommissionUp(BigDecimal commissionUp) {
+        this.commissionUp = commissionUp;
+    }
+
+    public BigDecimal getCommissionDown() {
+        return commissionDown;
+    }
+
+    public void setCommissionDown(BigDecimal commissionDown) {
+        this.commissionDown = commissionDown;
+    }
+
+    public int getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(int merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public Boolean getUcell() {
+        return this.id == 1000;
+    }
+
+    public Boolean getBeeline() {
+        return this.id == 100;
+    }
+
+    public Boolean getWebmoney() {
+        return this.id == 95;
+    }
+
+    public Boolean getQiwi() {
+        return this.id == 96;
+    }
 }
