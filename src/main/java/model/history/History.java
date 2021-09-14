63fa3c1e309da446f63f10b85c79e502a893d209
Musobek253 <musobek253@gmail.com>
2021-09-14 11:16:18 +0300

    P2p

--- src/main/java/model/history/History.java
+++ src/main/java/model/history/History.java
@@ -1,5 +1,19 @@
 package model.history;
 
-public class History {
+import model.base.BaseModel;
+
+import java.util.UUID;
+
+public class History extends BaseModel {
+ private UUID p2pId;
+ private UUID paymentId;
+ private UUID userId;
+
+    public History(String name, UUID p2pId, UUID paymentId, UUID userId) {
+        super(name);
+        this.p2pId = p2pId;
+        this.paymentId = paymentId;
+        this.userId = userId;
+    }
 
 }
